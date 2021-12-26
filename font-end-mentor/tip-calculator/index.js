90b61f5d00ad688f5c47eefd60588b1cd389b4b7
hlaingmyohein <96315235+hlaingmyohein@users.noreply.github.com>
2021-12-26 22:43:15 +0630

    Update index.js

--- font-end-mentor/tip-calculator/index.js
+++ font-end-mentor/tip-calculator/index.js
@@ -1,83 +1,9 @@
-function Calc(data,no){
-    var bill = document.querySelector('.input-bill').value;
-    var people = document.querySelector('.input-people').value;
-    
-    if(bill == 0){
-        document.querySelector('.bill-err').innerHTML = "Can't be zero";
-        document.querySelector('.bill-form').classList.add('err-border');
-    }else if(people==0){
-        document.querySelector('.people-form ').classList.add('err-border');
-        document.querySelector('.people-err').innerHTML = "Can't be zero";
-    }
-    else{
-        document.querySelector('.bill-err').innerHTML = "";
-        document.querySelector('.bill-form').classList.remove('err-border');
+let data1 = "0"
 
-        document.querySelector('.people-form').classList.remove('err-border');
-        document.querySelector('.people-err').innerHTML = "";
+let bill = "0"
 
-        document.querySelector('.custom-input').value = "Custom";
-        
-        document.querySelector('.no6').classList.remove('err-border');
+let people = "0"
 
-        for(var i = 1; i < 6; i++){
-            document.querySelector('.no'+i).classList.remove('active');
-        }
-        document.querySelector('.'+no).classList.add('active');
-       
-        var tip = data * bill / 100;
-        // console.log(tip);
-
-        var total = bill / people + tip;
-        // console.log(total);
-
-        document.querySelector('.tip-ans').innerHTML = '$'+tip.toFixed(3);
-        document.querySelector('.total-ans').innerHTML = '$'+total.toFixed(3);
-    }
-}
-
-function CustomCalc(){
-    var bill = document.querySelector('.input-bill').value;
-    var people = document.querySelector('.input-people').value;
-    var data = document.querySelector('.custom-input').value;
-
-    if(data <= 0){
-        // console.log('error');
-        document.querySelector('.no6').classList.add('err-border');
-    }
-    else{
-        if(bill == 0){
-            document.querySelector('.bill-err').innerHTML = "Can't be zero";
-            document.querySelector('.bill-form').classList.add('err-border');
-        }else if(people==0){
-            document.querySelector('.people-form ').classList.add('err-border');
-            document.querySelector('.people-err').innerHTML = "Can't be zero";
-        }
-        else{
-            document.querySelector('.bill-err').innerHTML = "";
-            document.querySelector('.bill-form').classList.remove('err-border');
-    
-            document.querySelector('.people-form').classList.remove('err-border');
-            document.querySelector('.people-err').innerHTML = "";
-
-            document.querySelector('.no6').classList.remove('err-border');
-    
-            for(var i = 1; i < 6; i++){
-                document.querySelector('.no'+i).classList.remove('active');
-            }
-
-            var tip = data * bill / 100;
-            // console.log(tip);
-    
-            var total = bill / people + tip;
-            // console.log(total);
-    
-            document.querySelector('.tip-ans').innerHTML = '$'+tip.toFixed(3);
-            document.querySelector('.total-ans').innerHTML = '$'+total.toFixed(3);
-        }
-    }
-    
-}
 
 function Reset(){
     window.location = '';
@@ -90,3 +16,109 @@ document.querySelector(".custom-input")
         CustomCalc();
     }
 });
+
+function Calc(bill,people,data){
+    // console.log(data);
+
+    var bill = bill;
+    var people = people;
+    if(people<=0){
+        document.querySelector('.tip-ans').innerHTML = '$0.00'; 
+        document.querySelector('.total-ans').innerHTML = '$0.00'; 
+    }
+    else{
+        var tip = data * bill / 100;
+        var total = bill / people + tip;
+
+        document.querySelector('.tip-ans').innerHTML = '$'+tip.toFixed(3);
+        document.querySelector('.total-ans').innerHTML = '$'+total.toFixed(3); 
+    }
+    
+}
+
+
+document.querySelector('.input-bill').addEventListener('change', function(evt) {
+    bill = this.value;
+    document.querySelector('.input-people').addEventListener('change',function(evt){
+            if(this.value<=0){
+                document.querySelector('.people-form ').classList.add('err-border');
+                document.querySelector('.people-err').innerHTML = "Can't be zero"; 
+            }
+            else{
+                // document.querySelector('.bill-err').innerHTML = "";
+                // document.querySelector('.bill-form').classList.remove('err-border');
+    
+                document.querySelector('.people-form ').classList.remove('err-border');
+                document.querySelector('.people-err').innerHTML = "";
+
+                document.querySelector('.custom-input').value = "Custom";
+
+                people = this.value;                     
+            }  
+    })
+    
+});
+
+
+document.querySelector('.no1').addEventListener('click',function(evt){
+    // console.log(this.value);
+    var data1 = this.value;
+    Calc(bill,people,parseInt(data1));
+    document.querySelector('.custom-input').value = "Custom";
+    for(var i = 1; i < 6; i++){
+        document.querySelector('.no'+i).classList.remove('active');
+    }
+    document.querySelector('.no1').classList.add('active');
+})
+
+document.querySelector('.no2').addEventListener('click',function(evt){
+    // console.log(this.value);
+    var data1 = this.value;
+    Calc(bill,people,parseInt(data1));
+    document.querySelector('.custom-input').value = "Custom";
+    for(var i = 1; i < 6; i++){
+        document.querySelector('.no'+i).classList.remove('active');
+    }
+    document.querySelector('.no2').classList.add('active');
+})
+
+document.querySelector('.no3').addEventListener('click',function(evt){
+    // console.log(this.value);
+    var data1 = this.value;
+   Calc(bill,people,parseInt(data1));
+   document.querySelector('.custom-input').value = "Custom";
+   for(var i = 1; i < 6; i++){
+    document.querySelector('.no'+i).classList.remove('active');
+    }
+    document.querySelector('.no3').classList.add('active');
+})
+
+document.querySelector('.no4').addEventListener('click',function(evt){
+    // console.log(this.value);
+    var data1 = this.value;
+   Calc(bill,people,parseInt(data1));
+   document.querySelector('.custom-input').value = "Custom";
+   for(var i = 1; i < 6; i++){
+    document.querySelector('.no'+i).classList.remove('active');
+    }
+    document.querySelector('.no4').classList.add('active');
+})
+
+document.querySelector('.no5').addEventListener('click',function(evt){
+    // console.log(this.value);
+    var data1 = this.value;
+   Calc(bill,people,parseInt(data1));
+   document.querySelector('.custom-input').value = "Custom";
+   for(var i = 1; i < 6; i++){
+    document.querySelector('.no'+i).classList.remove('active');
+    }
+    document.querySelector('.no5').classList.add('active');
+})
+
+document.querySelector('.custom-input').addEventListener('change',function(evt){
+    var data1 = this.value;
+    Calc(bill,people,parseInt(data1));
+    for(var i = 1; i < 6; i++){
+        document.querySelector('.no'+i).classList.remove('active');
+    }
+})
